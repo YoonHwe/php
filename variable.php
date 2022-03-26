a1cf6e0e38ba859478cda8416d29c7646d5b3ec6
YoonHwe <dbsghl676@gmail.com>
2022-03-27 02:00:16 +0900

    Create variable.php

---
+++ variable.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <body>
+       <h1>Variables</h1>
+       
+       <?php
+       $name = "egoing";
+       echo "Lorem ipsum dolor $name sit amet consectetur adipisicing elit. Adipisci, $name accusamus! Illum quisquam quia enim, cupiditate facere, provident doloribus, saepe aliquid numquam id quos sequi $name quidem in hic quibusdam! Ullam, $name soluta!";
+       ?>
+       
+    </body>
+</html>
